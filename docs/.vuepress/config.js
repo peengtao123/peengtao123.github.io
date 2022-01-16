d2d12a7e6573e330f7f2f8d3ec0484feb279f5b0
pengtao <peengtao123@126.com>
2022-01-16 20:35:48 +0800

    connect

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,7 @@
 module.exports = {
-  title: 'Hello VuePress',
-  description: 'Just playing around'
+  title: '架构教程',
+  description: 'Just playing around',
+  head: [
+      ['link', { rel: 'icon', href: '/logo.png' }]
+    ]
 }
