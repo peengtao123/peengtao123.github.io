d89c6944c224eebd59006e36d63f2d3954b5ced7
奔小康 <peengtao123@126.com>
2026-04-08 21:41:41 +0800

    int

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ import { defineUserConfig } from 'vuepress'
 import { viteBundler } from '@vuepress/bundler-vite'
 
 export default defineUserConfig({
-  base: '/aaa/',
+  base: '/',
   lang: 'zh-CN',
 
   title: '丑八怪',
